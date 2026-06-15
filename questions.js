84c1790ba2112ae267abb76305b1eccd8352d7de
M. Emre Ak <54697813+mehmetemreak@users.noreply.github.com>
2026-06-15 04:58:07 +0300

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,10 @@
+{
+  "difficulty": "easy|medium|hard",
+  "points": 1|2|3,
+  "text": "soru metni",
+  "choices": ["A","B","C","D"],
+  "answer": 0,
+  "hint": "tek cümle ipucu",
+  "fact": "soru sonrası bilgi kartı",
+  "tags": ["tag1","tag2"]
+}
